f5fba5c2cfcb935bc1fef2012a8d030da553c4b4
Siddhi <siddhimahadik257@gmail.com>
2022-08-17 01:39:11 +0530

    Completed Javascript Logic

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let newsSection=document.querySelector(".news-section")
+let searchBox=document.querySelector(".searchbox")
+let btn=document.querySelector(".btn1")
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2ad3a07c92msh5cf39f06f19c686p1e0f34jsncec7afa34e32',
+        'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com'
+    }
+};
 function getNews() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '2ad3a07c92msh5cf39f06f19c686p1e0f34jsncec7afa34e32',
-            'X-RapidAPI-Host': 'newscatcher.p.rapidapi.com'
-        }
-    };
     fetch("https://newscatcher.p.rapidapi.com/v1/latest_headlines?lang=en&media=True", options)
     .then(response => response.json()).then(response=>{
         let htmlstring=""
@@ -19,7 +21,7 @@ function getNews() {
                <div class="info">
                    <h3>${item.title}</h3>
                    <p class="info-des">${item.summary.substr(0,120)+"..."}</p>
-                   <button class="btn2">Read</button>
+                   <button class="btn2"><a href=${item.link}  > Read </a></button>
                </div>
            </div>
            `
@@ -28,3 +30,30 @@ function getNews() {
     }).catch(error=>console.log(error))
 }
 getNews()
+
+function searchNews(){
+    
+     fetch(`https://newscatcher.p.rapidapi.com/v1/search?q=${searchBox.value}&lang=en&sort_by=relevancy&page=1&media=True`,options).then(response=>response.json()).then(response=>{
+        let htmlstring=""
+        response.articles.map(item=>{
+            htmlstring+=`<div class="single-card">
+            <div class="card-image">
+                <img src="${item.media}"
+                    alt="">
+            </div>
+            <div class="info">
+                <h3>${item.title}</h3>
+                <p class="info-des">${item.summary.substr(0,120)+"..."}</p>
+                <button class="btn2"><a href=${item.link}> Read </a></button>
+            </div>
+        </div>`
+        })
+        newsSection.innerHTML=htmlstring
+        
+
+        console.log(response)
+     }).catch(error=>console.log(error))
+     
+}
+
+btn.addEventListener("click",searchNews)
